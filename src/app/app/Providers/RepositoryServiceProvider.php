2a9435a32a762f0c7006ccea7bc4173bf3ba90cc
DESKTOP-MH7J4TI\User <ahmed.inara@eg.aictec.com>
2023-04-11 17:56:12 +0200

    bind repository service interface

--- src/app/app/Providers/RepositoryServiceProvider.php
+++ src/app/app/Providers/RepositoryServiceProvider.php
@@ -6,7 +6,12 @@ use App\Repository\AuthRepository;
 use App\Repository\AuthRepositoryInterface;
 use App\Repository\BaseRepository;
 use App\Repository\EloquentRepositoryInterface;
-
+use App\Repository\ReservationRepository;
+use App\Repository\ReservationRepositoryInterface;
+use App\Repository\SeatRepository;
+use App\Repository\SeatRepositoryInterface;
+use App\Repository\TripRepository;
+use App\Repository\TripRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -20,6 +25,9 @@ class RepositoryServiceProvider extends ServiceProvider
     {
         $this->app->bind(EloquentRepositoryInterface::class, BaseRepository::class);
         $this->app->bind(AuthRepositoryInterface::class, AuthRepository::class);
+        $this->app->bind(ReservationRepositoryInterface::class, ReservationRepository::class);
+        $this->app->bind(TripRepositoryInterface::class, TripRepository::class);
+        $this->app->bind(SeatRepositoryInterface::class, SeatRepository::class);
 
     }
 
