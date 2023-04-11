b280ae608874ac2cadbd9235d2ab88ffb97bd4c4
DESKTOP-MH7J4TI\User <ahmed.inara@eg.aictec.com>
2023-04-11 17:52:16 +0200

    add bus and from station to station relations in trip model

--- src/app/app/Models/Trip.php
+++ src/app/app/Models/Trip.php
@@ -11,6 +11,16 @@ class Trip extends Model
     protected $guarded=[];
 
     public function stations(){
-        return $this->belongsToMany(Station::class , 'trip_stations')->withPivot('order');
+        return $this->belongsToMany(Station::class ,'trip_stations')->withPivot('order');
+    }
+    public function bus(){
+        return $this->belongsTo(Bus::class)->select('id','bus_number');
+    }
+    public function fromStation(){
+        return $this->belongsTo(Station::class,'start_station_id')->select('id','name');
+    }
+    public function toStation(){
+        return $this->belongsTo(Station::class,'end_station_id')->select('id','name');
     }
 }
+
